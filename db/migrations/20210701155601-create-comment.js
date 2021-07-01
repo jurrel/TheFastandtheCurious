7b53bdd978f7603d3fe120e00bbae49158eca550
colin <colin.goswell@gmail.com>
2021-07-01 09:08:13 -0700

    created comments model, migrated and seeded

---
+++ db/migrations/20210701155601-create-comment.js
@@ -0,0 +1,41 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Comments', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      description: {
+        allowNull: false,
+        type: Sequelize.TEXT,
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {model: 'Users'}
+      },
+      postId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {model: 'Posts'}
+      },
+      commentLikes: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Comments');
+  }
+};
