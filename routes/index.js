34c9b2c7a36f139e1a1c076901bca79f02241b04
colin <colin.goswell@gmail.com>
2021-07-01 14:48:43 -0700

    merge conflicts routes/index

--- routes/index.js
+++ routes/index.js
@@ -15,16 +15,9 @@ const { requireAuth } = require('../auth');
 /* GET home page. */
 router.get('/', csrfProtection, asyncHandler(async (req, res, next) => {
 
-<<<<<<< HEAD
   const posts = await db.Post.findAll({order: [["updatedAt", "desc"]]})
   const tags = await db.Tag.findAll();
   const users = await db.User.findAll();
-=======
-  const posts = await db.Post.findAll({})
-  const tags = await db.Tag.findAll();
-  const users = await db.User.findAll();
-
->>>>>>> comments
 
   res.render('index', { title: 'The Fast and the Curious  --  A webpage designed for speed freaks!', posts, tags, users });
 
