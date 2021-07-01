c42218ee4e5b9fe0888627bd200bf9387a6c3213
colin <colin.goswell@gmail.com>
2021-07-01 01:12:21 -0700

    eventlistener now working

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,25 @@
-window.addEventListener("load", (event)=>{
-    console.log("hello from javascript!")
-})
+
+const edit_button = document.querySelector('.edit-button');
+
+
+edit_button.addEventListener('click', async (e) => {
+    e.preventDefault();
+    const body = document.body;
+    try {
+        const res = await fetch('http://localhost:8080/', {
+            method: 'GET',
+
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+
+        edit_button.innerHTML = 'working now'
+
+
+    } catch (err) {
+        console.log('hello')
+        console.log(err)
+    }
+
+    })
